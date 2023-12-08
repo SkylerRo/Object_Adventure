89232f67c2a8fe35287ed1b51b99b7eec6e18586
SkylerRo <149194025+SkylerRo@users.noreply.github.com>
2023-12-08 15:35:12 -0500

    part2

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    health: 20,
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      inventory: ["Sunglasses", "Tiny Hat"],
+    },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  }
+  
 
 
-    
-   
 
-    const adventurer = {
-        name: "Robin",
-        health: 10,
-        inventory: ["sword", "potion", "artifact"],
-        companion: {
-            name: "Leo",
-            type: "Cat",
-            roll (mod = 0) {
-                const result = Math.floor(Math.random() * 20) + 1 + mod;
-                console.log(`${this.name} rolled a ${result}.`);
-        }
-        }
-        // loop inventory for robin
-        for (let i = 0; i < adventurer.inventory.length; i++) {
-            console.log(adventurer.inventory[i]);
-        }
+class adventurer extends character {
+    constructor(name,role,companion) {
+        super(name);
+        this.companion = companion;
+        this.role = role;
+    }
+}
 
-        const companion1 = {
-            name: "Leo",
-            health: 20,
-            companion2: {
-                companion: {
-                    name: "Frank",
-                    type: "Flea",
-                    inventory2: ["Sunglasses", "Tiny Hat"]
-                }
-            }
-        }
+class Adventurer extends Character {
+    constructor (name, role) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
